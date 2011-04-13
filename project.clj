b0baeadd309a81efc1d7c53d5b82d3c7484f1221
Jim Blomo <jblomo@yelp.com>
2011-04-13 15:09:43 -0700

    working

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject oddjob "0.0.1-SNAPSHOT"
+            :description "Hadoop utilities for MrJob that must run in the JVM"
+            :dependencies [[org.clojure/clojure "1.2.1"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [org.apache.hadoop/hadoop-streaming "0.18.3"]]
+            :dev-dependencies [[org.apache.hadoop/hadoop-core-with-dependencies "0.18.3"]
+                               [swank-clojure "1.2.1"]]
+            :main oddjob.core
+            :aot :all)
---
+++ test/oddjob/test/core.clj
@@ -0,0 +1,6 @@
+(ns oddjob.test.core
+  (:use [oddjob.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
