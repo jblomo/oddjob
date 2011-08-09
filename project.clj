5aebad20faad2bd39e393d96c00a8159ec2f1e82
Jim Blomo <jim.blomo+github@gmail.com>
2011-08-08 18:51:31 -0700

    works when manually running with -libjars oddjob-0.20.0-SNAPSHOT-standalone.jar

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject oddjob "0.0.3"
+(defproject oddjob "0.20.0-SNAPSHOT"
             :description "Hadoop utilities for MrJob that must run in the JVM"
             :dependencies [[org.clojure/clojure "1.2.1"]
-                           [org.clojure/clojure-contrib "1.2.0"]
-                           [org.apache.hadoop/hadoop-streaming "0.18.3"]]
-            :dev-dependencies [[org.apache.hadoop/hadoop-core-with-dependencies "0.18.3"]
+                           [org.clojure/clojure-contrib "1.2.0"]]
+            :dev-dependencies [[org.apache.hadoop/hadoop-streaming "0.20.2"]
+                               #_[org.apache.hadoop/hadoop-core-with-dependencies "0.20.2"]
                                [swank-clojure "1.2.1"]]
-            :main oddjob.core
             :aot :all)
--- src/oddjob/core.clj
+++
@@ -1,6 +0,0 @@
-(ns oddjob.core
-  (:gen-class))
-
-(defn -main
-  [& args]
-  (org.apache.hadoop.streaming.HadoopStreaming/main (into-array String args)))
