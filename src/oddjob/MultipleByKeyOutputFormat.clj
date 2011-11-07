cd693ceed32d2c47e41e8d5dd1e75624bcd9e1ec
dwilson <dwilson@buildfax.com>
2011-11-07 15:09:57 -0500

    write normal key/value with output named by key
    in the simplest case of using MultipleOutputFormat, we may still want
    full key/value pairs but just to keep them organized a bit differently.

---
+++ src/oddjob/MultipleByKeyOutputFormat.clj
@@ -0,0 +1,15 @@
+(ns oddjob.MultipleByKeyOutputFormat
+  (:import [org.apache.hadoop.fs Path]
+           [java.util StringTokenizer])
+  ; static class oddjob.MultipleCSVValueOutputFormat extends MultipleTextOutputFormat<Text, Text> {
+  (:gen-class :extends org.apache.hadoop.mapred.lib.MultipleTextOutputFormat))
+
+(defn -generateFileNameForKeyValue
+  "Generate the file output file name based on the given key and the leaf file
+  name.
+
+  This function joins the partial-path with the leaf file (typically part-0000x).
+  Developer is responsible for ensuring the resulting path still passes 
+  FSNamesystem.isValidName."
+  [this akey value leaf]
+  (str (Path. (str akey) leaf)))
